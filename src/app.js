0e6a0fb8b58176fc45f1f8f2268883ff88957f71
Borbély Gergő Árpád <borbelygergo06@gmail.com>
2023-03-23 09:50:32 +0100

    Calculation added to app.js and connection added to index.html

---
+++ src/app.js
@@ -0,0 +1,12 @@
+var betetInput = document.querySelector("#betet");
+var nevlegesInput = document.querySelector("#nevleges");
+var tokesitesekSzamaInput = document.querySelector("#tokesitesekSzama");
+var szamolasBtn = document.querySelector("#szamolas");
+var evesKamatOutput = document.querySelector("#evesKamat");
+
+function evesKamatSzamolas() {
+    let tenyleges = (Math.pow((1 + Number(nevlegesInput.value) / (100 * Number(tokesitesekSzamaInput.value))), Number(tokesitesekSzamaInput.value))-1) * Number(betetInput.value);
+
+    evesKamatOutput.value = tenyleges;
+    return false;
+}
